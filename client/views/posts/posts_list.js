51c0006d724b861dee1ee466bbb671fb6061b8ca
davewalk <daviddwalk@gmail.com>
2013-05-26 00:32:52 -0400

    Removing static data b/c app data ccoming from db now

--- client/views/posts/posts_list.js
+++ client/views/posts/posts_list.js
@@ -1,21 +1,3 @@
-var postsData = [
-    {
-        title: 'The Cavs are open to a trade? Sure, we’ll oblige...',
-        author: 'Kevin Hetrick',
-        url: 'http://www.cavstheblog.com/?p=19975'
-    },
-    {
-        title: 'Watch Queens Of The Stone Age play "...Like Clockwork" in full at Los Angeles show',
-        author: 'unknown',
-        url: 'http://www.nme.com/news/queens-of-the-stone-age/70504'
-    },
-    {
-        title: 'Arrested Development on Netflix',
-        author: 'none',
-        url: 'http://movies.netflix.com/movie/Arrested-Development/70140358?mqso=81003611'
-    }
-];
-
 Template.postsList.helpers({
     posts: function () {
         return Posts.find();
